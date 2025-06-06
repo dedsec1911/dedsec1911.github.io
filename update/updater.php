c64c22f51b0c37aba65d7177aa98f5f56a7c4bfe
Harshul <harshu40116@gmail.com>
2025-06-06 17:37:30 +0530

    Update updater.php

--- update/updater.php
+++ update/updater.php
@@ -3,9 +3,9 @@ header('Content-Type: application/json');
 
 // Example hardcoded update info
 $updateInfo = [
-    "versionCode" => 5,
-    "versionName" => "1.0.5",
-    "apkUrl" => "https://yourdomain.com/app-latest.apk"
+    "versionCode" => 2,
+    "versionName" => "1.0",
+    "apkUrl" => "https://dedsec1911.github.io/app-latest.apk"
 ];
 
 echo json_encode($updateInfo);
